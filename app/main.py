0bbcc2e713d36e52695bf6c7329227a8dcce9f41
kislaya1996 <kislaya@neufin.co>
2026-07-14 11:40:47 +0530

    Removed unused dependency from router

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 from fastapi import Depends, FastAPI
 
-from app.dependencies import get_query_token, get_token_header
 from app.internal import admin
 from app.routers import tasks, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(tasks.router)
 app.include_router(
     admin.router,
-    dependencies=[Depends(get_token_header)],
     responses={418: {"description": "I'm a teapot"}},
 )
 
